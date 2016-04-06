8f3d2c107b5ace4c000b676dfb2d5b76a20bd42d
James Lukensow <jlukensow@gmail.com>
2016-04-06 10:45:21 -0600

    Testing Codeship deploy.

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ for(var i = 0; i < number_of_rooms; i++) {
   rooms[i] = {count: 0, player_id_having_the_ball: 1}; //, round_started: false};
 }
 
-
 function get_list_of_rooms() {
   var list_of_rooms = {number_of_rooms: number_of_rooms, rooms: []}
   for(var i = 0; i < number_of_rooms; i++) {
